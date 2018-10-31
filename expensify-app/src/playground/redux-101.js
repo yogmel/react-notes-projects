fbe4da03d8a684dcd526d8f77bc9f5997df95fef
yogmel <pong7219@gmail.com>
2018-10-31 11:18:18 -0300

    Redux - final setting

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,16 +1,39 @@
 import { createStore } from 'redux'
 
-const store = createStore((state = { count: 0 }, action) => {
+// Action generators - functions that return action objects
+
+const incrementCount = ( {incrementBy = 1} = {} ) => ({
+  type: 'INCREMENT',
+  incrementBy
+})
+
+const decrementCount = ({decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+})
+
+const resetCount = () => ({
+  type: 'RESET',
+})
+
+const setCount = ({count}) => ({
+  type: 'SET',
+  count
+})
+
+// Reducers
+// 1. Reducers are pure functions. Pure functions output only its input, that is, its return doesn't depend on a variable outside the function's scope.
+// 2. Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       }
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       }
     case 'RESET':
       return {
@@ -23,37 +46,25 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
         return state
   }
-})
+}
+
+const store = createStore(countReducer)
 
 const unsubscribe = store.subscribe(() => { // subscribe runs the function every time the state is changed
   console.log(store.getState())
 })
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-})
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// })
 
-store.dispatch({
-  type: 'INCREMENT'
-})
+store.dispatch(incrementCount())
+store.dispatch(incrementCount({incrementBy: 5}))
 
-store.dispatch({ 
-  type: 'DECREMENT',
-  decrementBy: 10
-})
+store.dispatch(decrementCount({decrementBy: 2}))
 
 // unsubscribe() // stops the update of subscribe
 
-store.dispatch({
-  type: 'RESET'
-})
-
-store.dispatch({
-  type: 'DECREMENT'
-})
-
-store.dispatch({
-  type: 'SET',
-  count: 101
-})
+store.dispatch(setCount({count: 13}))
+store.dispatch(resetCount())
